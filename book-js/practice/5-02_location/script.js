cf0c0016f3f2cb87aa7bd58bac2ebd8aa22479bc
ykskks <fkt.kohei@gmail.com>
2019-05-18 20:07:42 +0900

    add files

---
+++ book-js/practice/5-02_location/script.js
@@ -0,0 +1,16 @@
+document.getElementById("form").select.onchange = function() {
+    location.href = document.getElementById("form").select.value;
+}
+
+var lang = document.querySelector("html").lang
+
+var opt;
+if (lang == "ja") {
+    opt = document.querySelector('option[value="index.html"]');
+} else if (lang == "en") {
+    opt = document.querySelector('option[value="index-en.html"]');
+} else if (lang == "zh") {
+    opt = document.querySelector('option[value="index-zh.html"]');
+}
+
+opt.selected = true;
